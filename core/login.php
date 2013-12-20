134f0b7ee5e0986b9759be1027ab7780c14ab3e7
Juan Jose Cordova Zamorano <jcordova@dsinteg.com>
2013-12-20 12:37:12 -0600

    Incluye modelo de usuario
    Incluye un ejemplo del modelo de usuarios

--- core/login.php
+++ core/login.php
@@ -6,6 +6,16 @@
 // si existe usuario y contraseña entonces genera código 
 //si no esta como usuario entonces error
 //require_once 'config.php';
-$html = file_get_contents('../html/serviciosmunicipalesft.html');
+//$SESION_USUARIO=new sesion_usuario();
+//if $EXITE_USUARIO.existe($usuario)..
+//$TIPO_USUARIO=new tipo_usuario($usuario);
+//$ESTADO_USUARIO
+//$MUNICIPIO_USUARIO
+//$LOCALIDAD_USUARIO
+//$OPTION_LOCALIDAD = generaoptionLocalidad($ESTADO,$MUNICIPIO,$LOCALIDAD); crea el código html segun los datos de estado,municipio,localidad
+// ejemplo 
+//<option value="1">TLAXCALA DE XICOTENCATL</option>
+//<option value="8">OCOTLAN</option>
+$html = file_get_contents('../html/serviciosmunicipales.html');
 echo $html;
 ?>
--- js/loadmap.js
+++ js/loadmap.js
@@ -1,12 +1,13 @@
 // DSInteg
 // Inicializa Mapa dependiendo las coordenadas
 
-function inicializamapa() {
-	var mapOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
+function inicializamapa(latitud,longitud) {
+		var mapOptions = {
+		center: new google.maps.LatLng(latitud,longitud),
 		zoom: 20,
 		mapTypeId: google.maps.MapTypeId.HYBRID
 	};
 	var map = new google.maps.Map(document.getElementById("map_canvas"),
 	mapOptions);
+
 }
